5ab3a40f02915b0c238b0eb349dbb620f31b07f0
keke <1551968007@qq.com>
2023-07-23 09:38:25 +0800

    hot-fix commit2

--- src/main/java/com/keke/git/gittest.java
+++ src/main/java/com/keke/git/gittest.java
@@ -6,5 +6,6 @@ public class gittest {
         System.out.println("hello git!2");
         System.out.println("hello git!3");
         System.out.println("hello git!4");
+        System.out.println("hot-fix test");
     }
 }
