46dcb28a04d299a2e126271eb4a2aeedb67a7384
keke <1551968007@qq.com>
2023-07-23 09:11:06 +0800

    second commit

--- src/main/java/com/keke/git/gittest.java
+++ src/main/java/com/keke/git/gittest.java
@@ -3,5 +3,6 @@ package com.keke.git;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!2");
     }
 }
